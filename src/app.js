aff10d28769035aed457736107e21082b39c7ad7
Gadi Okoth <gadi.okoth@gmail.com>
2019-08-02 11:34:10 +0300

    Setup app on Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast')
 
 const app = express();
+const port = process.env.PORT ||3000;
 
 //Define paths for Express
 const publicDirectoryPath = path.join(__dirname,'../public');
@@ -74,8 +75,8 @@ app.get('/weather',(req,res)=>{
                         location: location,
                         address: req.query.address
                     });*/
-                    console.log('Location is '+location)
-                    console.log('Data', forecastData);
+                   // console.log('Location is '+location)
+                    //console.log('Data', forecastData);
                 }          
               });
          }  
@@ -98,6 +99,6 @@ app.get('*',(req,res)=>{
     })
 });
 
-app.listen(3000,()=>{
-    console.log('Server is listening at port 3000');
+app.listen(port,()=>{
+    console.log(`Server is listening at port ${port}`);
 });
